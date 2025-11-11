5d0d0964d92738bcde8e289fbe892dcbaf25e39a
Nishi Mehta <93097701+inishimehta@users.noreply.github.com>
2025-11-10 22:36:14 -0500

    Lab Week 10 Completed.

---
+++ Lab10/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UserForm from './components/UserForm';
+import UserSignupForm from './components/UserSignupForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Week10 | React Form and Events example</h1>
+      <UserForm/>
+      <UserSignupForm />
+    </div>
+  );
+}
+
+export default App;
